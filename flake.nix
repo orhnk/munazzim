a5d06b7c6c8a5c034fee812c2306a657d9800f2c
orhnk <utfeightt@gmail.com>
2026-01-31 07:03:53 +0300

    fix typo

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
             set -euo pipefail
 
             detect_terminal() {
-              if [ -n "${TERMINAL-}" ] && command -v "${TERMINAL}" >/dev/null 2>&1; then
+              if [ -n "${TERMINAL}" ] && command -v "${TERMINAL}" >/dev/null 2>&1; then
                 echo "${TERMINAL}"
                 return 0
               fi
